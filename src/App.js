2ff6e5a87d0c8959478449c12a7de9839fa2b163
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-02-26 21:12:25 +0200

    Add an aircraft booking form component. Add mui, react router, datefns and axios Remove some default react project files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import AircraftBookingForm from "./components/AircraftBookingForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Helsinki East Aviation Club</h1>
+      <AircraftBookingForm />
     </div>
   );
 }
---
+++ src/components/AircraftBookingForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+function AircraftBookingForm() {
+    const [booking, setValues] = useState({ 
+        aircraft: '',
+        firstName: '',
+        lastName: '',
+        phone: '',
+        startTime: '',
+        endTime: '',
+    });
+
+    const [message, setMessage] = useState('');
+
+    //Temporary solution, doesn't actually add anything, database to be added
+    const addBooking = (e) => {
+
+        e.preventDefault();
+
+        //if any field is empty show message
+        if (!(booking.firstName && booking.lastName && booking.phone && booking.startTime && booking.endTime)) {
+            setMessage('Please fill all the fields.');
+        
+        //reset all user input fields when clicking submit
+        } else {
+            setValues( { firstName: '', lastName: '', phone: '', startTime: '', endTime: '' } );
+            setMessage('');
+        }
+    }
+
+
+    const change = (e) => {
+        setValues({
+        ...booking, [e.target.name]: e.target.value
+        });
+    };
+
+    return (
+        <div>
+        <h2>Book an aircraft</h2>
+        <form>
+            <label htmlFor="aircraft">aircraft:</label>
+            <select name="aircraft" value={ booking.aircraft } onChange={ (e) => change(e) }>
+                <option value="OH-CAB (C152)">OH-CAB (C152)</option>
+                <option value="OH-CYZ (C172)">OH-CYZ (C172)</option>
+            </select><br />
+
+            <label htmlFor="firstName">first name:</label>
+            <input type="text" name="firstName" value={ booking.firstName } onChange={ (e) => change(e) }/><br />
+        
+            <label htmlFor="lastName">last name:</label>
+            <input type="text" name="lastName" value={ booking.lastName } onChange={ (e) => change(e) }/><br />
+        
+            <label htmlFor="phone">phone number:</label>
+            <input type="text" name="phone" value={ booking.phone } onChange={ (e) => change(e) }/><br />
+        
+            <label htmlFor="startTime">start time:</label>
+            <input type="datetime-local" name="startTime" value={ booking.startTime } onChange={ (e) => change(e) }/><br />
+
+            <label htmlFor="endTime">end time:</label>
+            <input type="datetime-local" name="endTime" value={ booking.endTime } onChange={ (e) => change(e) }/><br />
+
+            
+            <input type='submit' value='Submit' onClick={ (e) => addBooking(e) }/>
+            </form>
+        <p>{ message }</p>
+        </div>
+    )
+}
+
+export default AircraftBookingForm;
